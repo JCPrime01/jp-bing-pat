73c4ed241833c704323d8442df8b61653fb2d3a1
JCPrime01 <272623760+JCPrime01@users.noreply.github.com>
2026-05-19 13:31:58 -0300

    chore: update shareCode to 5033880EJTK

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/zy1z1vkz?shareCode=I6QB1RP3D9L&afp_campaign=bingo' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/zy1z1vkz?shareCode=5033880EJTK&afp_campaign=bingo' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
