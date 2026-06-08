61c2b907b2de85f5c69628bdced0a4f138169de9
JCPrime01 <vanzokiller@gmail.com>
2026-06-08 10:34:39 -0300

    chore: shareCode KACFW31AUJK -> AYXDB5T2SYW (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/zy1z1vkz?shareCode=KACFW31AUJK&afp_campaign=bingo' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/zy1z1vkz?shareCode=AYXDB5T2SYW&afp_campaign=bingo' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
