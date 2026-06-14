d8373d353103f699539920c57c576f70fea49110
JCPrime01 <vanzokiller@gmail.com>
2026-06-14 10:08:11 -0300

    chore: shareCode WMD3D5NVNOX -> HOEK2YL5HUI (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/zy1z1vkz?shareCode=WMD3D5NVNOX&afp_campaign=bingo' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/zy1z1vkz?shareCode=HOEK2YL5HUI&afp_campaign=bingo' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
