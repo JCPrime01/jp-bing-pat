f1b90ebb5a8a6a2d6a9fa01eb4a628a9e04f8f3d
JCPrime01 <vanzokiller@gmail.com>
2026-06-15 09:25:17 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/zy1z1vkz?shareCode=BM2QP85LISS&afp_campaign=bingo' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7053&shareCode=BM2QP85LISS&afp1_campaign=bingo' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
