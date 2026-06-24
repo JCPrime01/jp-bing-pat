aea8f00baade93fe96c269fd95a7dbcd84c5e098
JCPrime01 <vanzokiller@gmail.com>
2026-06-23 23:56:57 -0300

    chore: shareCode BONEGNFX7CJ -> 96QSUBJPEN3 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7053&shareCode=BONEGNFX7CJ&afp1_campaign=bingo' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7053&shareCode=96QSUBJPEN3&afp1_campaign=bingo' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
